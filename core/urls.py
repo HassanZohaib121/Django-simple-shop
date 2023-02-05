9d6430abeb5320917d5607b5306eca526ed71d93
Hassan Zohaib <78634222+HassanZohaib121@users.noreply.github.com>
2023-02-05 12:19:40 +0500

    Add files via upload

--- core/urls.py
+++ core/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html',
          authentication_form=LoginForm), name='login'),
+    path('log-out/', auth_views.LogoutView.as_view(), name='logout'),
 ]
